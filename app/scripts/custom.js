37d8f7655e2f32f4acaa2a813330d1fe9b02127e
Anton Sozontov <a.sozontov@gmail.com>
2016-10-15 19:11:38 +0700

    Update products

--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -6,6 +6,19 @@ jQuery(function() {
     let index = $(e.currentTarget).index();
     backgrounds.removeClass('shown')
     $(backgrounds[index]).addClass('shown')
-  })
+  });
+
+  $(window).on('slideChange', function(event, number, element){
+    setInterval(function(){
+      if($('.slideshow').hasClass('selected')){
+        let events = 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend';
+
+        let previousBackground = $('.slideshow.selected .background.shown');
+        let nextBackground = $('.slideshow.selected .background.shown').nextOrFirst('.background');
+        nextBackground.addClass('shown');
+        previousBackground.removeClass('shown');
+      }
+    }, 5000);
+  });
 })
 
